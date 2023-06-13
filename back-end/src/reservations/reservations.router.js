5c557c6316ce454839ebb144fd11f9b8248be8f9
Jon Mendoza <jonmendoza1824@gmail.com>
2023-06-13 07:53:52 -0400

    adds asyncErrorBoundary and methodNotAllowed to errors folder, and updates router.js

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -3,10 +3,11 @@
  *
  * @type {Router}
  */
-
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methondNotAllowed = require("../errors/methodNotAllowed");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
