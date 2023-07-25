860f4990cb5cf1a3ad05527a79b4ae1aad438588
Jon Mendoza <jonmendoza1824@gmail.com>
2023-07-25 16:10:03 -0400

    adds search() to service to filter reservations by mobile number

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -29,7 +29,15 @@ function update(updatedReservation){
     .update(updatedReservation, "*")
     .returning("*")
     .then(updatedReservation => updatedReservation[0])
+}
 
+function search(mobile_number) {
+    return knex("reservations")
+    .whereRaw(
+    "translate(mobile_number, '() -', '') like ?",
+    `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
 }
 
 module.exports = {
@@ -37,4 +45,5 @@ module.exports = {
     list,
     create,
     update,
+    search,
 }
