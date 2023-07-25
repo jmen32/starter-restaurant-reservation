95c50e09ffc9d8928bf70093ec3d35a1f7c80e95
Jon Mendoza <jonmendoza1824@gmail.com>
2023-07-25 16:14:53 -0400

    adds CreateSearch to handle filtering reesrvation searches by phone number

---
+++ front-end/src/search/CreateSearch.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { listReservations } from '../utils/api'
+import ReservationCard from '../reservations/ReservationCard.js'
+
+export default function CreateSearch() {
+    const [reservations, setReservations] = useState([])
+    const [mobile_number, setMobileNumber] = useState('')
+
+    const handleChange = ({target}) => {
+        setMobileNumber(target.value)
+    }
+
+    const handleSubmit = async (event) => {
+        try{
+            event.preventDefault();
+            const filteredReservations = await listReservations({mobile_number})
+            setReservations(filteredReservations)
+            setMobileNumber(''); 
+        }catch(error){
+            console.error(error)
+    }
+}
+
+    return (
+        <div>
+            <h1>Search</h1>
+            <p>Search :  
+            <input 
+                name="mobile_number"
+                className='form-control'
+                placeholder="Enter a customer's phone number"
+                value={mobile_number}
+                onChange={handleChange}
+                type='search' 
+            /> 
+            </p>
+            <button onClick={handleSubmit} type="submit">Find</button>
+
+        <h3>reservations</h3>
+
+        {reservations.length ? 
+        <div>
+        {reservations.map((reservation) => (
+            <div key={reservation.reservation_id}>
+            <ReservationCard reservation={reservation}/>
+            </div>
+        ))}
+        </div>
+        :
+        'No reservations found'
+        }
+        </div>
+    )
+}
