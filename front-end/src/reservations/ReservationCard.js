9a39d08110dfa9a57ef55639e43bd5e7603fd83b
Jon Mendoza <jonmendoza1824@gmail.com>
2023-08-01 05:08:59 -0400

    updates time to display on a 12hr format

--- front-end/src/reservations/ReservationCard.js
+++ front-end/src/reservations/ReservationCard.js
@@ -2,8 +2,27 @@ import React from 'react'
 import { formatAsTime } from '../utils/date-time'
 
 export default function ReservationCard({reservation}) {
+  function loadTime(time) {
+    const resTime = formatAsTime(time);
+    const [hours, minutes] = resTime.split(':');
+    const timeAsNumber = parseInt(hours) * 60 + parseInt(minutes);
 
-  const resTime = formatAsTime(reservation.reservation_time)
+    if (timeAsNumber < 720) {
+      // Before 12:00 PM
+      return `${resTime} AM`;
+    } else if (timeAsNumber === 720) {
+      // 12:00 PM
+      return `${resTime} PM`;
+    } else {
+      // After 12:00 PM
+      const pmHours = parseInt(hours) - 12;
+      const pmTime = `${pmHours.toString().padStart(2, '0')}:${minutes.padStart(2, '0')}`;
+      return `${pmTime} PM`;
+    }
+  }
+
+  console.log(loadTime(reservation.reservation_time))
+  
 
   return (
     <div className="card" style={{width: "16rem"}}>
@@ -16,7 +35,7 @@ export default function ReservationCard({reservation}) {
         <p>Name: {reservation.last_name}, {reservation.first_name}</p>
         <p>Party Size: {reservation.people}</p>
         <p>Date: {reservation.reservation_date}</p>
-        <p>Time: {resTime}</p>
+        <p>Time: {loadTime(reservation.reservation_time)}</p>
         <p>Mobile Number: {reservation.mobile_number}</p>
       </div>
     </div>
